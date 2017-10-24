d6d8a72ba9c27021262fa68e84c6fb971923570a
Mehar <p.s.chhabra@ieee.org>
2017-10-24 23:33:47 +0800

    Fully Functional TCP Port Scanner with port input handling

--- pyNeM.py
+++ pyNeM.py
@@ -18,35 +18,95 @@ print '\n'
 
 
 #Port input checking and port scanning
-if str(inputport.isdigit()) == 'True':
+
+#--------------------------------------------- Scan Single Port -----------------------------------------------------------
+if str(inputport.isdigit()) == 'True':                  
     # Main port scanning with some error handling
     t1 = datetime.now() #Time of scan initiation
-
-    try:
-        for port in range(1,1025):  
-            sock = socket(AF_INET, SOCK_STREAM)
-            result = sock.connect_ex((remoteServerIP, port))
-            if result == 0:
-                print "Port {}: 	 Open".format(port)
-            sock.close()
-
-    except KeyboardInterrupt:
-        print "Keyboard Interrupt Detected"
-        exit()
-
-    except gaierror:
-        print 'Hostname could not be resolved. Exiting'
-        exit()
-
-    except error:
-        print "Unable to connect to server"
-        exit()
     
-else if str(inputport.isdigit()) == 'False'
-    print "F"
+    if int(inputport) <= 65535:
+        try:
+            sock = socket(AF_INET, SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((remoteServerIP, int(inputport)))
+            if result == 0:
+                print "Port {}: 	 Open".format(inputport)
+                sock.close()
 
+        except KeyboardInterrupt:
+            print "Keyboard Interrupt Detected"
+            exit()
 
+        except gaierror:
+            print 'Hostname could not be resolved. Exiting'
+            exit()
 
+        except error:
+            print "Unable to connect to server"
+            exit()
+    else:
+        print "Invalid port number. Port not in range"
+       
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+#------------------------------------------ Scan Specified Range of Ports ------------------------------------------------
+
+else:
+    if str(inputport).find('-') != -1:   #
+        ports = str(inputport).split('-')   #Store minimum and maximum port number in this list
+        minport = int(ports[0])
+        maxport = int(ports[1])
+        if (minport > maxport) or (minport <=0) or (maxport > 65535) or (minport == maxport): #Error check
+            print "Invalid port range."
+        else:
+            t1 = datetime.now() #Time of scan initiation
+            try:
+                for inputport in range(minport,maxport):    
+                    sock = socket(AF_INET, SOCK_STREAM)
+                    sock.settimeout(1)
+                    result = sock.connect_ex((remoteServerIP, inputport))
+                    if result == 0:
+                        print "Port {}: 	 Open".format(inputport)
+                        sock.close()
+
+            except KeyboardInterrupt:
+                print "Keyboard Interrupt Detected"
+                exit()
+
+            except gaierror:
+                print 'Hostname could not be resolved. Exiting'
+                exit()
+
+            except error:
+                print "Unable to connect to server"
+                exit()
+    
+    
+#---------------------------------------------- Default Ports ----------------------------------------------------    
+    else: 
+        try:
+            t1 = datetime.now() #Time of scan initiation
+            for inputport in range(1,1000):
+                sock = socket(AF_INET, SOCK_STREAM)
+                sock.settimeout(1)
+                result = sock.connect_ex((remoteServerIP, int(inputport)))
+                if result == 0:
+                    print "Port {}: 	 Open".format(inputport)
+                    sock.close()
+
+        except KeyboardInterrupt:
+            print "Keyboard Interrupt Detected"
+            exit()
+
+        except gaierror:
+            print 'Hostname could not be resolved. Exiting'
+            exit()
+
+        except error:
+            print "Unable to connect to server"
+            exit()
+        
 
 # Calculates the time difference, for execution time
 t2 = datetime.now() #Get current time
